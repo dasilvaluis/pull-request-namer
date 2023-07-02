b2fc6c81235a77666060e703e1c94e08f7c98828
dasilvaluis <15066801+dasilvaluis@users.noreply.github.com>
2023-07-02 23:17:12 +0300

    Fix bug with empty space before ticket id

--- src/components/TicketInput.vue
+++ src/components/TicketInput.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import { makeBranchName, makeRequestTitle } from '@/lib/ticketFormatter'
 import { isolateTicketId } from '@/lib/ticketParser'
+import { ref } from 'vue';
 
 const emit = defineEmits(['update:modelValue'])
+const modelValue = ref()
 
 const makeTicketData = (event: Event) => {
   const ticketName = (event.target as HTMLInputElement).value
 
-  if (!ticketName) {
+  const trimmedTicketName = ticketName.trim()
+
+  if (!trimmedTicketName) {
     emit('update:modelValue', undefined)
     return
   }
 
-  const [title, id] = isolateTicketId(ticketName)
+  const [title, id] = isolateTicketId(trimmedTicketName)
 
   const branchName = makeBranchName(title, id)
   const requestTitle = makeRequestTitle(title, id)
@@ -31,9 +35,11 @@ const makeTicketData = (event: Event) => {
       name="ticket"
       type="text"
       id="ticket-input"
-      placeholder="copy & paste your ticket here"
+      placeholder="type ..."
+      v-model="modelValue"
       @input="makeTicketData"
     />
+    <small v-if="!modelValue">Example: XYZ-1234 Implement foobar</small>
   </div>
 </template>
 
